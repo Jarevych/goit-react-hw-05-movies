81aa17f55e1217ff773d198f9bbfa2b7bbd89833
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-10-20 20:59:05 +0300

    cl

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -16,7 +16,7 @@ export default function MovieDetails() {
   const [movieData, setMovieData] = useState(null);
   const location = useLocation();
   const goBackTo = useRef(location.state?.from ?? '/');
-  console.log('Before navigation:', goBackTo);
+  // console.log('Before navigation:', goBackTo);
 
   const defaultImg =
     'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700';
